b211adbe2c8cae1b82e6f2c25f2d341f648f8935
kevinyin9 <kevinyin999@gmail.com>
2024-07-15 09:35:58 +0000

    update shit

--- binance_server.py
+++ binance_server.py
@@ -20,7 +20,8 @@ import crypto_relative_strength
 from pluto.exchange import BinanceExchange
 from utils import setup_logger
 
-logger_path = f".\\log\\log.txt"
+os.makedirs('log', exist_ok=True)
+logger_path = f".\log\log.txt"
 logger = setup_logger(name=__name__,
                       file_dir=logger_path,
                       is_file_handled=True)
@@ -32,9 +33,9 @@ bot = telebot.TeleBot(TOKEN)
 class BinanceServer:
     
     def __init__(self):
-        self.whitelist, self.data_in_memory = [], {}
+        self.whitelist = []
+        self.data_in_memory = {}
         self.fetch_white_list_flag = False
-        self.alert_list = {}
         self.current_subscriptions = set()
         self.new_whitelist_flag = False # if has new whitelist, update websocket subscription.
         self.base_url = "http://43.207.214.219"
@@ -49,28 +50,7 @@ class BinanceServer:
         self.binance_exchange = BinanceExchange(api_key="tnXg5o95Zu8wrpaRdy7xslZ9qWiR7Esb3DX4GAPpQZn1tg9Z9P2mNtiK6H4ldT87",
                                           secret="PAkEQ0qEr8khxLjEyB5oZdUHwhmqCEgrqp9DXoReBRt8l2MzTRtOZvo05SGHdeZ2")
         
-        logger.info("Current balance: ", asyncio.run(self.binance_exchange.get_balance()))
-
-    def get_top_n(self, n):
-        df = pd.read_csv('rs_value.csv', index_col=0)
-
-        def extract_symbol_quantity(cell_value):
-            symbol, rs_value = cell_value.split('_')
-            return symbol, float(rs_value)
-
-        top_n_dict = {}
-
-        len_col = len(df.columns)
-        for _, row in df.iterrows():
-            date = row['date']
-            # for column in df.columns[1:n+2]: # get weakest top-n
-            for column in df.columns[len_col - n:len_col]: # get strongest top-n
-                cell = row[column]
-                symbol, rs_value = extract_symbol_quantity(cell)
-                if symbol not in top_n_dict:
-                    top_n_dict[symbol] = []
-                top_n_dict[symbol].append(date)
-        return top_n_dict
+        logger.info(f"Current balance: {asyncio.run(self.binance_exchange.get_balance())}")
 
     # Function to fetch kline data from Binance
     def fetch_klines(self, symbol, interval, start_time=None, end_time=None):
@@ -111,29 +91,25 @@ class BinanceServer:
 
     # Function to update local kline data and load into memory
     def update_local_data(self):
-        # print(f"update_local_data")
+        logger.info("start update_local_data")
         for symbol in self.whitelist:
             df_local = self.load_local_data(symbol)
+
             if not df_local.empty:
                 last_timestamp = df_local['datetime'].iloc[-1].to_pydatetime()
                 last_timestamp_ms = int(time.mktime(last_timestamp.timetuple()) * 1000)
             else:
                 last_timestamp_ms = None
             
-            # df_new = self.fetch_klines(symbol, "1h", start_time=last_timestamp_ms)
-            # print(df_new)
-            # print('df_new')
-            # df_updated = pd.concat([df_local, df_new])
-            # df_updated = df_updated.tail(20)  # Keep only the latest 20 rows
-            # self.save_local_data(symbol, df_updated)
-            # self.data_in_memory[symbol] = df_updated
-            # print(data_in_memory[symbol])
-            print(f"Updated {symbol} kline data")
+            df_new = self.fetch_klines(symbol, "1h", start_time=last_timestamp_ms)
+            df_updated = pd.concat([df_local, df_new])
+            self.data_in_memory[symbol] = df_updated
+            logger.info(f"Updated {symbol} kline data")
 
 # Function to handle incoming websocket messages
     def on_message(self, ws, message):
         data = json.loads(message)
-        print(data)
+        logger.debug(data)
         if 'k' in data:
             symbol = data['s']
             if symbol in self.whitelist:
@@ -156,27 +132,20 @@ class BinanceServer:
                     df_updated = pd.concat([self.data_in_memory[symbol], df])
                 # df_updated = df_updated.tail(20)  # Keep only the latest 20 rows
                 # print(df_updated)
-                df_updated = long_bband_tp(df_updated[-25:])
+                df_updated = long_bband_tp(df_updated[-25:], self.position[symbol])
                 signal = df_updated['signal'].iloc[-1]
                 if signal != 0:
                     close_price = round(df['close'].iloc[-1], self.precision_map[symbol]['price_presicion'])
                     qty = round(self.margin_per_trade / close_price, self.precision_map[symbol]['qty_presicion'])
-                    if signal == 1:
+                    if self.position[symbol] == 0 and signal == 1:
                         order = asyncio.run(self.binance_exchange.place_order(symbol, "LIMIT", "LONG", "BUY", close_price, qty))
                         logger.info(f"create new order: {order}")
-                    elif signal == -1:
+                        bot.send_message(CHAT_ID, f"{symbol} buy long order.")
+                        # TODO: filled user ws, change self.position
+                    elif self.position[symbol] == 1 and signal == -1:
                         order = asyncio.run(self.binance_exchange.place_order(symbol, "LIMIT", "LONG", "SELL", close_price, qty))
                         logger.info(f"create new order: {order}")
-                    
-                # print(df_updated)
-                last_row = df_updated.iloc[-1]
-                if last_row.close < last_row['Lower Band'] and self.alert_list[symbol] == False:
-                    self.alert_list[symbol] = True
-                    bot.send_message(CHAT_ID, f"{symbol} touches lower band.")
-                # save_local_data(symbol, df_updated)
-                # print(df.tail(2))
-                # print('---')
-                # print(f"{symbol} Bollinger Bands:\n", df[['SMA', 'Upper Band', 'Lower Band']].tail(1))
+                        bot.send_message(CHAT_ID, f"{symbol} sell long order.")
 
     def on_error(self, ws, error):
         print("Error: ", error)
@@ -197,12 +166,13 @@ class BinanceServer:
             data = response.json()
             for item in data['symbols']:
                 if item['symbol'] in symbols:
+                    symbol = item['symbol']
                     self.precision_map[symbol] = {}
                     for filter in item['filters']:
                         if(filter['filterType'] == 'PRICE_FILTER'):
-                            self.precision_map[symbol]['price_precision'] = math.log(filter['tickSize'], 10)
+                            self.precision_map[symbol]['price_precision'] = math.log(float(filter['tickSize']), 10)
                         if(filter['filterType'] == 'MARKET_LOT_SIZE'):
-                            self.precision_map[symbol]['qty_precision'] = math.log(filter['stepSize'], 10)
+                            self.precision_map[symbol]['qty_precision'] = math.log(float(filter['stepSize']), 10)
                     logger.info(f"{symbol} price precision: {self.precision_map[symbol]['price_precision']}, qty precision: {self.precision_map[symbol]['qty_precision']}")
         else:
             print(f"Error: {response.status_code} - {response.text}")
@@ -237,14 +207,17 @@ class BinanceServer:
                 pairs_to_unsubscribe = self.current_subscriptions - new_subscriptions
                 pairs_to_subscribe = new_subscriptions - self.current_subscriptions
                 
-                ws.send(json.dumps({
-                    "method": "UNSUBSCRIBE",
-                    "params": list(pairs_to_unsubscribe),
-                    "id": 312
-                }))
+                logger.info(f"pairs_to_unsubscribe: {pairs_to_unsubscribe}")
+                if len(pairs_to_unsubscribe) != 0:
+                    ws.send(json.dumps({
+                        "method": "UNSUBSCRIBE",
+                        "params": list(pairs_to_unsubscribe),
+                        "id": 312
+                    }))
                 
                 time.sleep(1)
                 
+                logger.info(f"pairs_to_subscribe: {pairs_to_subscribe}")
                 ws.send(json.dumps({
                     "method": "SUBSCRIBE",
                     "params": list(pairs_to_subscribe),
@@ -263,15 +236,6 @@ class BinanceServer:
                 
                 self.new_whitelist_flag = False
             
-            time.sleep(0.5)
-
-
-    def reset_alert_list(self):
-        while True:
-            now = datetime.now()
-            if now.minute == 0 and now.second == 1:
-                for symbol in self.whitelist:
-                    self.alert_list[symbol] = False
             time.sleep(0.5)
 
     def update_whitelist(self):
@@ -294,14 +258,13 @@ class BinanceServer:
             if first_update_whitelist or (now.hour % 8 == 0 and now.minute == 0 and now.second < 3):
                 first_update_whitelist = False
                 tmp = set()
-                crypto_relative_strength.main(False, start_date, end_date, no_download, exclude_symbols, send_msg=True)
+                crypto_relative_strength.main(False, start_date, end_date, no_download, exclude_symbols, send_msg=False)
                 
                 for time_interval in ['4h', '8h', '24h']:
                     df = pd.read_csv(f'rs_value_{time_interval}.csv', index_col=0)
 
                     len_col = len(df.columns)
                     for _, row in df.iterrows():
-                        date = row['date']
                         # for column in df.columns[1:n+2]: # get weakest top-n
                         for column in df.columns[len_col - 5:len_col]: # get strongest top-n
                             cell = row[column]
@@ -309,13 +272,13 @@ class BinanceServer:
                             if rs_value == 0:
                                 continue
                             tmp.add(symbol)
+                            self.position[symbol] = 0
                 
                 self.whitelist = list(tmp)
                 self.get_precision(self.whitelist)
                 print(f"whitelist: {self.whitelist}")
-                self.data_in_memory = {symbol for symbol in self.whitelist}
                 bot.send_message(CHAT_ID, f"whitelist: {self.whitelist}")
-                # self.update_local_data()
+                self.update_local_data()
                 self.fetch_white_list_flag = True
                 self.new_whitelist_flag = True
             time.sleep(0.5)
@@ -327,23 +290,9 @@ if __name__ == '__main__':
     t1.daemon = True
     t1.start()
 
-    # Run websocket in a separate thread
     t2 = Thread(target=binanceServer.start_websocket)
     t2.daemon = True
     t2.start()
     
-    for symbol in binanceServer.whitelist:
-        binanceServer.alert_list[symbol] = False
-    t3 = Thread(target=binanceServer.reset_alert_list)
-    t3.daemon = True
-    t3.start()
-    
     t1.join()
     t2.join()
-    t3.join()
-    
-    
-
-    # thread = Thread(target=get_top_n, args=(5, ))
-    # thread.daemon = True
-    # thread.start()
--- strategy_long.py
+++ strategy_long.py
@@ -1,5 +1,8 @@
 import talib
 import numpy as np
+import pandas as pd
+
+pd.options.mode.chained_assignment = None
 
 def long_atr_tp(df):
     df['upper_band'], df['middle_band'], df['lower_band'] = talib.BBANDS(df['close'], timeperiod=20)
@@ -50,7 +53,7 @@ def long_atr_tp(df):
             
     return df
             
-def long_bband_tp(df):
+def long_bband_tp_backtest(df):
     df['upper_band'], df['middle_band'], df['lower_band'] = talib.BBANDS(df['close'], timeperiod=20)
 
     # 计算ATR
@@ -69,23 +72,55 @@ def long_bband_tp(df):
         # if df['close'].iloc[i] < df['lower_band'].iloc[i] and df.at[df.index[i], 'can_entry'] == 1 and df['position'].iloc[i-1] == 0:
         
         # 爆量，碰下軌
-        if df['volume'].iloc[i] > df['volume'].iloc[i-1] * 2 and df['close'].iloc[i] < df['lower_band'].iloc[i] and df.at[df.index[i], 'can_entry'] == 1 and df['position'].iloc[i-1] == 0:
-            df.at[df.index[i], 'signal'] = 1  # 開多頭倉位
-            df.at[df.index[i], 'stop_loss'] = df['close'].iloc[i] - 2 * df['ATR'].iloc[i]  # 設置止损價格
+        if df['volume'].iloc[i] > df['volume'].iloc[i-1] * 2 and df['close'].iloc[i] < df['lower_band'].iloc[i] and df.loc[df.index[i], 'can_entry'] == 1 and df['position'].iloc[i-1] == 0:
+            df.loc[df.index[i], 'signal'] = 1  # 開多頭倉位
+            df.loc[df.index[i], 'stop_loss'] = df['close'].iloc[i] - 2 * df['ATR'].iloc[i]  # 設置止损價格
         elif df['position'].iloc[i-1] == 1 and df['close'].iloc[i] > df['upper_band'].iloc[i]:
-            df.at[df.index[i], 'signal'] = -1  # 平仓
+            df.loc[df.index[i], 'signal'] = -1  # 平仓
         else:
-            df.at[df.index[i], 'stop_loss'] = df['stop_loss'].iloc[i-1]
+            df.loc[df.index[i], 'stop_loss'] = df['stop_loss'].iloc[i-1]
 
         # 止損
         if df['position'].iloc[i-1] == 1 and df['low'].iloc[i] < df['stop_loss'].iloc[i-1]:
-            df.at[df.index[i], 'signal'] = -1  # 平仓
+            df.loc[df.index[i], 'signal'] = -1  # 平仓
 
         # 更新倉位
         if df['position'].iloc[i-1] == 1 and df['signal'].iloc[i] == 1:
-            df.at[df.index[i], 'position'] = df['position'].iloc[i-1]
+            df.loc[df.index[i], 'position'] = df['position'].iloc[i-1]
         else:
-            df.at[df.index[i], 'position'] = df['position'].iloc[i-1] + df['signal'].iloc[i]
+            df.loc[df.index[i], 'position'] = df['position'].iloc[i-1] + df['signal'].iloc[i]
+            
+    return df
+
+def long_bband_tp(df, position):
+    df['upper_band'], df['middle_band'], df['lower_band'] = talib.BBANDS(df['close'], timeperiod=20)
+
+    # 计算ATR
+    df['ATR'] = talib.ATR(df['high'], df['low'], df['close'], timeperiod=14)
+
+    # 初始化信号和仓位
+    df = df.assign(signal = 0)
+    df = df.assign(can_entry = 1)
+    df = df.assign(take_profit = np.nan)
+    df = df.assign(stop_loss = np.nan)
+
+    # 生成交易信号和止损条件
+    for i in range(1, len(df)):
+        # 沒爆量條件，碰下軌
+        # if df['close'].iloc[i] < df['lower_band'].iloc[i] and df.at[df.index[i], 'can_entry'] == 1 and df['position'].iloc[i-1] == 0:
+        
+        # 爆量，碰下軌
+        if df['volume'].iloc[i] > df['volume'].iloc[i-1] * 2 and df['close'].iloc[i] < df['lower_band'].iloc[i] and df.loc[df.index[i], 'can_entry'] == 1 and position == 0:
+            df.loc[df.index[i], 'signal'] = 1  # 開多頭倉位
+            df.loc[df.index[i], 'stop_loss'] = df['close'].iloc[i] - 2 * df['ATR'].iloc[i]  # 設置止损價格
+        elif position == 1 and df['close'].iloc[i] > df['upper_band'].iloc[i]:
+            df.loc[df.index[i], 'signal'] = -1  # 平仓
+        else:
+            df.loc[df.index[i], 'stop_loss'] = df['stop_loss'].iloc[i-1]
+
+        # 止損
+        if position == 1 and df['low'].iloc[i] < df['stop_loss'].iloc[i-1]:
+            df.loc[df.index[i], 'signal'] = -1  # 平仓
             
     return df
 
